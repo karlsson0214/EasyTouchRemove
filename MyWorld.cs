32a862cd9bd173265bbcc239a84c24e29c39f519
Rikard Karlsson <karlsson0214@gmail.com>
2024-11-11 15:52:23 +0100

    first

--- MyWorld.cs
+++ MyWorld.cs
@@ -4,15 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EasyMonoGame;
+using EasyTouchRemove;
 
 namespace EasyStart
 {
     internal class MyWorld : World
     {
-        public MyWorld() : base(600, 800)
+        public MyWorld() : base(800, 600)
         {
             // Tile background with the file "bluerock" in the Content folder.
-            BackgroundTileName = "bluerock";
-        }  
+            BackgroundTileName = "sand2";
+
+            Add(new Turtle(), "turtle2", 400, 300);
+            Add(new Plum(), "plum", 200, 150);
+            Add(new Plum(), "plum", 200, 450);
+            Add(new Plum(), "plum", 600, 150);
+            Add(new Plum(), "plum", 600, 450);
+        }
     }
 }
---
+++ Turtle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMonoGame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace EasyTouchRemove
+{
+    internal class Turtle : Actor
+    {
+        private float rotationSpeed = 90f;
+        private float speed = 200f;
+        public override void Update(GameTime gameTime)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                // turn left
+                Turn(-rotationSpeed * deltaTime);
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                // turn right
+                Turn(rotationSpeed * deltaTime);
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                // move forward
+                var distance = speed * deltaTime;
+                Move(distance);
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                // move backward at half speed
+                var distance = speed / 2 * deltaTime;
+                Move(-distance);
+            }
+            if (IsTouching(typeof(Plum)))
+            {
+                RemoveTouching(typeof(Plum));
+            }
+        }
+
+    }
+}
